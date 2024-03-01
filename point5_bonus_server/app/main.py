cb672fe59d2b568f10ed4ca9fe1b38c4ac7c682b
Tatiana García <51728851+TatianaGarcia1128@users.noreply.github.com>
2024-03-01 14:56:17 -0500

    Documentación de la clase del modelo y el main

--- point5_bonus_server/app/main.py
+++ point5_bonus_server/app/main.py
@@ -1,3 +1,4 @@
+#import modules
 from fastapi import FastAPI, WebSocket, Request, UploadFile, File, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
@@ -5,33 +6,44 @@ from fastapi.staticfiles import StaticFiles
 from app.models.question_answer_paraphrase import EMBEDDINGS
 import os
 
+#Create FastAPI instance
 app = FastAPI()
+
+# Set up Jinja2 templates
 template = Jinja2Templates(directory="app/templates")
 
+# Mount static files
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+# Create an instance of the EMBEDDINGS class
 model = EMBEDDINGS()
 
-
+# Define the root endpoint
 @app.get("/")
 async def index(request: Request):
     return template.TemplateResponse("index.html", {"request": request, "title": "Chat embeddings"})
 
+# Define the WebSocket endpoint
 @app.websocket("/message")
 async def websocket_endpoint(websocket: WebSocket):
+    # Accept the WebSocket connection
     await websocket.accept()
     print('hola')
     while True:
+        # Receive a question from the WebSocket
         question = await websocket.receive_text()
         print(question)
+        # Search for paragraphs similar to the question
         answers = model.search_paragraphs(question, filename)
         print(answers)
+        # Send the question and the answers back to the WebSocket
         await websocket.send_text(f"\nQuestion: {question}")
         for answer in answers:
             print(answer)
             await websocket.send_text(f"Result: {answer}")
 
 
+# Define the file upload endpoint
 @app.post("/file")
 async def upload_file(file: UploadFile = File(...)):
     UPLOADS_DIRECTORY = "app/files"
@@ -39,8 +51,9 @@ async def upload_file(file: UploadFile = File(...)):
         #definition filepath
         file_path = os.path.join(UPLOADS_DIRECTORY, file.filename)
 
+        #Delete existing file if it already exists
         if os.path.exists(file_path):
-            os.remove(file_path)  # Eliminar el archivo existente si ya existe
+            os.remove(file_path)  
     
         # Read the binary content
         content = await file.read()
@@ -54,15 +67,17 @@ async def upload_file(file: UploadFile = File(...)):
         print(file.filename)
         global filename 
         filename = file.filename
+        # Search for embeddings in the content of the file
         model.search_embeddings_content(file.filename)
 
     except Exception as e:
+        # Raise an HTTP exception if an error occurs
         raise HTTPException(status_code=500, detail=str(e))
 
+    # Return a JSON response indicating that the file was saved
     return JSONResponse(content={"saved": True}, status_code=200)
 
+# Run the application if this file is the entry point
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=80)
-
-
--- point5_bonus_server/app/models/question_answer_paraphrase.py
+++ point5_bonus_server/app/models/question_answer_paraphrase.py
@@ -7,31 +7,61 @@ import numpy as np
 import os
 
 class EMBEDDINGS:
-        #Constructor
+    """
+    This class is used to generate and manipulate embeddings using the Qdrant client.
+    """
+
+    #construction
     def __init__(self):
+        """
+        Initializes the EMBEDDINGS class.
+
+        Params:
+            None
+        """
         self.qdrant_client = qdrant_client.QdrantClient(":memory:")
         self.content = None
         self.created_collections = set()
 
     def _get_paragraphs(self, filename):
+        """
+        Reads a file and splits the content into paragraphs.
 
-        # Obtener el directorio del script actual
+        Params:
+            filename (str): The name of the file to read.
+
+        Returns:
+            paragraphs (list): A list of paragraphs from the file.
+        """
+
+        #Get current script directory
         script_dir = os.path.dirname(os.path.abspath(__file__))
         script_dir =script_dir.replace('models','files')
         file_path  = os.path.join(script_dir, filename)
 
-        # Reading text
+        #Reading text
         with open(file_path, 'r', encoding='utf-8') as archivo:
             # Lee el contenido del archivo y lo almacena en una variable
             content = archivo.read()
 
-        # Eliminar saltos de línea
+        # Remove line breaks
         self.content = content.replace('\n', ' ')
         # Split the text into paragraphs based on double line breaks ('\n\n')
         paragraphs = self.content.split('.')
         return paragraphs
+    
 
-    def _model_embedding(self, content):  
+    def _model_embedding(self, content):
+        """
+        Generates embeddings for the given content using SentenceTransformer.
+
+        Params:
+            content (str): The content to generate embeddings for.
+
+        Returns:
+            embeddings (numpy.ndarray): The generated embeddings.
+        """
+
         model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-mpnet-base-v2')
         embeddings = model.encode(content)
         return embeddings
@@ -39,11 +69,20 @@ class EMBEDDINGS:
 
         # Index the embeddings into Qdrant
     def _qdrant_collection(self):
+        """
+        Creates a new collection in Qdrant or overwrites an existing one.
+
+        Params:
+            None
+
+        Returns:
+            None
+        """
         EMBEDDING_SIZE = 768  # 512 for small variant
         collection_name = "MyCollection"
 
         if collection_name in self.created_collections:
-            # La colección ya existe, podemos optar por sobrescribirla o realizar alguna otra acción
+            #The collection already exists, we can choose to overwrite it or perform some other action
             self.qdrant_client.delete_collection(collection_name)
 
         self.qdrant_client.create_collection(
@@ -51,11 +90,20 @@ class EMBEDDINGS:
             vectors_config=VectorParams(size=EMBEDDING_SIZE, distance=Distance.COSINE),
         )
 
-        # Agregar el nombre de la colección a la lista de colecciones creadas
+        #Add the collection name to the list of created collections
         self.created_collections.add(collection_name)
 
 
     def _update_vectors(self, embeddings):
+        """
+        Updates the vectors in the Qdrant collection with the given embeddings.
+
+        Params:
+            embeddings (numpy.ndarray): The embeddings to update the vectors with.
+
+        Returns:
+            None
+        """
         self.qdrant_client.upsert(
             collection_name="MyCollection",
             points=Batch(
@@ -65,26 +113,54 @@ class EMBEDDINGS:
         )
 
     def _search_embeddings(self, consult):
+        """
+        Searches for embeddings in the Qdrant collection that are similar to the given consult.
+
+        Params:
+            consult (str): The consult to search for similar embeddings.
+
+        Returns:
+            results (list): A list of search results.
+        """
         embeddings = self._model_embedding(consult)
         results = self.qdrant_client.search(
             collection_name="MyCollection",
             query_vector = np.squeeze(embeddings),
-            limit=3  # Obtener los 10 vectores más similares
+            limit=3
             )
         
         return results
     
     
     def search_embeddings_content(self, filename):
+        """
+        Reads a file, generates embeddings for the content, creates a Qdrant collection, and updates the vectors with the embeddings.
+
+        Params:
+            filename (str): The name of the file to read.
+
+        Returns:
+            None
+        """
         paragraphs = self._get_paragraphs(filename)
         embeddings = self._model_embedding(paragraphs)
         self._qdrant_collection()
         self._update_vectors(embeddings)
     
     def search_paragraphs(self, consult,filename):
+        """
+        Searches for paragraphs in a file that are similar to the given consult.
+
+        Params:
+            consult (str): The consult to search for similar paragraphs.
+            filename (str): The name of the file to search in.
+
+        Returns:
+            answers (list): A list of paragraphs that are similar to the consult.
+        """
         print('consult', consult)
         results = self._search_embeddings(consult)
-        # Dividir la cadena por comas y obtener la parte que contiene el ID
+        #Split the string by commas and get the part that contains the ID
         id_vectors = []
         for resultado in results:
             id_vector = resultado.id
@@ -92,21 +168,9 @@ class EMBEDDINGS:
 
         paragraphs = self._get_paragraphs(filename)
         answers = []
-        # Imprimir la respuesta
+        #Print the answer
         for id in id_vectors:
             print("Respuesta:", paragraphs[id])
             answers.append(paragraphs[id])
 
         return answers
-
-
-        #https://huggingface.co/sentence-transformers/paraphrase-multilingual-mpnet-base-v2
-            
-
-# consulta = ['Dónde se produjo una explosión']
-# # consulta = ['Cuál es la melodia que le gustaba a Johny']
-# file_path='Stephen King - Cell (1).txt'
-# model = EMBEDDINGS()
-# model.search_embeddings_content(file_path)
-# model.search_paragraphs(consulta, file_path)
-#         # print('embedding consulta:', embedding_consulta)
